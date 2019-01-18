c52705050031c4e4373d7d4f5c38ddb5df6ad5ea
gillilands19 <gillilands19@gmail.com>
2019-01-17 16:33:18 -0800

    Creates CPP factorial Solution

---
+++ CPP_Factorial/factorial.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+long long recursiveFactorial(int n);
+long long factorialLoop(int n);
+
+int main()
+{
+  int n;
+  int x;
+
+  cout << "Please enter a number: " << endl;
+  cin >> n;
+  cout << "Please enter another number: " << endl;
+  cin >> x;
+
+  cout << "The Factorial of " << n << " is " << recursiveFactorial(n) << endl;
+
+  cout << "The Factorial of " << x << " is " << factorialLoop(x) << endl;
+
+  return 0;
+}
+
+long long recursiveFactorial(int n)
+{
+  if(n > 1)
+  {
+    return n * recursiveFactorial(n - 1);
+  }
+  else if(n == 1)
+  {
+    return 1;
+  }
+  else
+  {
+    cout << "Not a Positive Number";
+    return 0;
+  }
+}
+
+long long factorialLoop(int n)
+{
+  long long factorial = 1;
+  for(int i = 1; i <= n; ++i)
+  {
+    factorial *= i;
+  }
+  if(n == 1)
+  {
+    return 1;
+  }
+  else if(n < 1)
+  {
+    cout << " Not a Positive Number ";
+    return 0;
+  }
+  else
+  {
+  return factorial;
+  }
+}
